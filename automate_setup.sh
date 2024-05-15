241f63fdb43ac6de1e4a9f3c3cfd4e4295e2a96b
nyetes <nitesh.mahar1208@gmail.com>
2024-05-15 23:22:28 +0545

    Final commit

--- automate_setup.sh
+++ automate_setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Define Docker Hub username
 DOCKERHUB_USERNAME="nyetes"
 
-# Install Docker
+
 install_docker() {
     echo "Installing Docker..."
-    # Update package index
+    
     sudo apt-get update
 
-    # Install packages to allow apt to use a repository over HTTPS
+   
     sudo apt-get install -y \
         apt-transport-https \
         ca-certificates \
@@ -17,29 +16,27 @@ install_docker() {
         gnupg-agent \
         software-properties-common
 
-    # Add Docker’s official GPG key
+   
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-    # Set up the stable Docker repository
+   
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-    # Update package index again
+   
     sudo apt-get update
 
-    # Install Docker CE
+    
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-    # Verify Docker installation
     sudo docker --version
 }
 
-# Clone GitHub repository
 clone_repository() {
     echo "Cloning GitHub repository..."
     git clone https://github.com/silarhi/php-hello-world.git
 }
 
-# Create Dockerfile for Apache and Nginx
+
 create_dockerfile() {
     echo "Creating Dockerfile..."
     echo "# Use the official PHP image with Apache" > Dockerfile
@@ -61,26 +58,26 @@ create_dockerfile() {
     echo "CMD service apache2 start && nginx" >> Dockerfile
 }
 
-# Build Docker image
+
 build_image() {
     echo "Building Docker image..."
     docker build -t php-hello-world .
 }
 
-# Push Docker image to Docker Hub
+
 push_image() {
     echo "Pushing Docker image to Docker Hub..."
-    # Log in to Docker Hub
+   
     docker login -u "$DOCKERHUB_USERNAME"
     
-    # Tag Docker image
+   
     docker tag php-hello-world "$DOCKERHUB_USERNAME/php-hello-world"
     
-    # Push Docker image to Docker Hub
+    
     docker push "$DOCKERHUB_USERNAME/php-hello-world"
 }
 
-# Main function
+
 main() {
     install_docker
     clone_repository
@@ -92,5 +89,5 @@ main() {
     push_image
 }
 
-# Run the main function
+
 main
